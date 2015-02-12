7d6cb3694340aa3ad339eac5d6d6d8fbb7b6ee08
sminutoli <sminutoli.it@gmail.com>
2015-02-12 15:16:41 -0300

    v0.0.12 released

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-/*! letrest - v0.0.11 - 2015-02-09 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
+/*! letrest - v0.0.12 - 2015-02-12 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
