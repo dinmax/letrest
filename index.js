1cca03dd33cf554268d9f277b1f4b33396424296
Matias Gonzalez <matias.gonzalez@dinmax.com>
2015-02-27 21:05:15 -0300

    v0.0.13 released

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-/*! letrest - v0.0.12 - 2015-02-12 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
+/*! letrest - v0.0.13 - 2015-02-27 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
