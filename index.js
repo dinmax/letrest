0a39c894876abf76642cd8991ec87727d5630c1d
Federico Salomone <federico.salomone@dinmax.com>
2015-11-17 16:41:55 -0300

    v0.0.34 released

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-/*! letrest - v0.0.33 - 2015-10-20 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
+/*! letrest - v0.0.34 - 2015-11-17 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
