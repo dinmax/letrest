d298b6d00de75a3112486a6f0e09506b26ebd135
sminutoli <sminutoli.it@gmail.com>
2015-11-23 19:09:29 -0300

    v0.0.35 released

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-/*! letrest - v0.0.34 - 2015-11-17 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
+/*! letrest - v0.0.35 - 2015-11-23 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
