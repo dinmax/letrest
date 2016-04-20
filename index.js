5388fd7fa3d6c962c1c980013f9314b463b1ec56
Matias Courtois <matiascoutois@Matiass-MacBook-Pro.local>
2016-04-20 13:55:40 -0300

    v0.2.0 released

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-/*! letrest - v0.1.1 - 2016-04-06 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
+/*! letrest - v0.2.0 - 2016-04-20 */module.exports.APP=require("./lib/Service.js"),module.exports.EDITOR=require("./lib/editor/index.js");
